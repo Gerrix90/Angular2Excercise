3b9e26b01de8be492b97ac7814e7afb5cc75e76b
Gerrix <jahja.trifunovic@gmail.com>
2017-03-07 23:15:35 +0100

    Template Form Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent }  from './app.component';
 import { UserComponent }  from './components/user.component';
@@ -9,7 +10,7 @@ import { ProbaComponent }  from './components/proba.component';
 import {routing} from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, routing, HttpModule],
+  imports:      [ BrowserModule, routing, HttpModule, FormsModule],
   declarations: [ AppComponent, AboutComponent, UserComponent, ProbaComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -4,12 +4,63 @@ import {PostService} from './service/post.service';
 @Component({
   selector: 'user',
   template: `<h1>User Component</h1>
+
+  <form novalidate #f="ngForm" (ngSubmit)="onSubmit(f); title.value=''; body.value=''">
+      <label>
+        <span>Title</span>
+        <input
+          type="text"
+          placeholder="Title"
+          name="title"
+          ngModel
+          #postItemTitle="ngModel"
+          #title
+          minlength="2"
+          required>
+      </label>
+      <div *ngIf="postItemTitle.errors?.required && postItemTitle.touched" >
+        Title is required
+       </div>
+       <div *ngIf="postItemTitle.errors?.minlength && postItemTitle.touched" >
+        Minimum of 2 characters
+        </div>
+        <label>
+        <span>Body</span>
+        <input
+          type="text"
+          placeholder="Body"
+          name="body"
+          ngModel
+          #postItemBody="ngModel"
+          #body
+          minlength="20"
+          required>
+      </label>
+      <div *ngIf="postItemBody.errors?.required && postItemBody.touched" >
+        Body is required
+       </div>
+       <div *ngIf="postItemBody.errors?.minlength && postItemBody.touched" >
+        Minimum of 20 characters
+        </div>
+      <button type="submit" [disabled]="f.invalid">Submit</button>
+    </form>
+    <hr>
+
   <proba [postItem]="posts"></proba>
   `,
   providers: [PostService]
 })
 export class UserComponent  {
   posts:Post[];
+  postItem: Post ={
+    id:0,
+    title:"",
+    body:""
+  };
+
+  onSubmit({value, valid}: {value:Post, valid:boolean}){
+    this.posts.push(value);
+  }
 
   constructor(private postService:PostService){
     this.postService.getPosts().subscribe(posts => {

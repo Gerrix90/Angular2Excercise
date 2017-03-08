14be31bb9772f894c7700619cf2793f39dfe9d8e
Gerrix <jahja.trifunovic@gmail.com>
2017-03-08 10:11:08 +0100

    Model-Driven

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpModule} from '@angular/http';
-import {FormsModule} from '@angular/forms'
+import {ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent }  from './app.component';
 import { UserComponent }  from './components/user.component';
@@ -10,7 +10,7 @@ import { ProbaComponent }  from './components/proba.component';
 import {routing} from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, routing, HttpModule, FormsModule],
+  imports:      [ BrowserModule, routing, HttpModule, ReactiveFormsModule],
   declarations: [ AppComponent, AboutComponent, UserComponent, ProbaComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -1,63 +1,53 @@
 import { Component } from '@angular/core';
 import {PostService} from './service/post.service';
+import {FormGroup, FormControl, Validators} from '@angular/forms'
 
 @Component({
   selector: 'user',
   template: `<h1>User Component</h1>
 
-  <form novalidate #f="ngForm" (ngSubmit)="onSubmit(f); title.value=''; body.value=''">
+  <form  [formGroup]="form" (ngSubmit)="onSubmit(form); title.value=''; body.value=''">
       <label>
         <span>Title</span>
         <input
           type="text"
           placeholder="Title"
           name="title"
-          ngModel
-          #postItemTitle="ngModel"
-          #title
-          minlength="2"
-          required>
+          minlength=3
+          formControlName="title"
+          #title>
       </label>
-      <div *ngIf="postItemTitle.errors?.required && postItemTitle.touched" >
-        Title is required
-       </div>
-       <div *ngIf="postItemTitle.errors?.minlength && postItemTitle.touched" >
-        Minimum of 2 characters
-        </div>
-        <label>
+      <div *ngIf="form.controls.title.errors?.pattern">
+      Invalid character!
+      </div>
+      <label>
         <span>Body</span>
         <input
           type="text"
           placeholder="Body"
           name="body"
-          ngModel
-          #postItemBody="ngModel"
-          #body
-          minlength="20"
-          required>
+          formControlName="body"
+          #body>
       </label>
-      <div *ngIf="postItemBody.errors?.required && postItemBody.touched" >
-        Body is required
-       </div>
-       <div *ngIf="postItemBody.errors?.minlength && postItemBody.touched" >
-        Minimum of 20 characters
-        </div>
-      <button type="submit" [disabled]="f.invalid">Submit</button>
+      <button type="submit" [disabled]="!form.valid">Submit</button>
     </form>
     <hr>
 
-  <proba [postItem]="posts"></proba>
+  <proba [postItem]="posts" ></proba>
   `,
   providers: [PostService]
 })
 export class UserComponent  {
+  form:any;
   posts:Post[];
-  postItem: Post ={
-    id:0,
-    title:"",
-    body:""
-  };
 
+  ngOnInit(){
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.compose([Validators.required, Validators.pattern('[\\w\\-\\s\\/]+')])),
+      body: new FormControl('')
+
+    });
+  }
   onSubmit({value, valid}: {value:Post, valid:boolean}){
     this.posts.push(value);
   }
